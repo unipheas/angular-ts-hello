bc338eb14d4f45edc6815fba4ef65408ee540a1e
Brian Phillips <brian.phillips@qq.com>
2017-08-05 17:16:39 +0800

    type script components

---
+++ point.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Point = (function () {
+    function Point(_x, _y) {
+        this._x = _x;
+        this._y = _y;
+    }
+    Point.prototype.draw = function () {
+        console.log("X: " + this._x + ", Y: " + this._y);
+    };
+    return Point;
+}());
+exports.Point = Point;
